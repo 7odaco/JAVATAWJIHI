3349f3727c1a34a633292613017708a269b3d6f0
7odaco <68192448+7odaco@users.noreply.github.com>
2024-05-16 18:09:32 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,24 @@
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in=new Scanner (System.in);
-int b=1;
-        int a;
-        a=in.nextInt();
-        for (int i=a;a>=1;a--){
 
 
-            b=a*b;
+        for (int i=1;i<=5;i++){
 
+            for (int j=1;j<=i;j++){
+
+                System.out.print("*");
+
+            }
+            System.out.println("");
         }
-        System.out.println(b);
 
+        for (int i = 4; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
 
     }
 }
